06096b244b3b882bc1bcd4bceb21717e4537875c
huyulin@calculatedfun.com <huyulin@calculatedfun.com>
2019-08-30 16:16:33 +0800

    ss

--- s/buss/wms/shipment/main/j/list.js
+++ s/buss/wms/shipment/main/j/list.js
@@ -22,11 +22,11 @@ $(function () {
 			name: "状态",
 			renderData: function (rowindex, data, rowdata, column) {
 				if (rowdata.delflag == 1) {
-					$("tr[d-tree='" + rowdata.dtee + "']").css("color", "#dedede");
+					$(`tr[d-tree='${rowdata.dtee}']`).css("color", "#dedede");
 					return "已删除";
 				} else {
 					if (data == 3) {
-						$("tr[d-tree='" + rowdata.dtee + "']").css("color", "red");
+						$(`tr[d-tree='${rowdata.dtee}]`).css("color", "red");
 					}
 				}
 				return gv.get("ACS_STATUS", data);
