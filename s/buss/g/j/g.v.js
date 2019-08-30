5979cdc768d33cccf522fd8b6a6637d797a81d49
huyulin@calculatedfun.com <huyulin@calculatedfun.com>
2019-08-30 15:51:54 +0800

    sss

--- s/buss/g/j/g.v.js
+++ s/buss/g/j/g.v.js
@@ -14,45 +14,40 @@ class gv {
 				localStorage.setItem("dic", str);
 				str = JSON.stringify(data.res);
 				localStorage.setItem("res", str);
-				this._readyNum++;
 			}
 			// 成功执行方法
 		});
 	}
 
-	static isReady() {
-		return this._readyNum >= 1;
-	}
-
 	static __getTS = (type) => {
-		if (!this._dic) { this._dic = jQuery.parseJSON(localStorage.getItem("dic")); }
-		return this._dic.filter(function (e) { return e.type == type; });
+		if (!gv._dic) { gv._dic = jQuery.parseJSON(localStorage.getItem("dic")); }
+		return gv._dic.filter(function (e) { return e.type == type; });
 	};
 
 	static get(type, key) {
-		var o = this.__getTS(type);
+		var o = gv.__getTS(type);
 		var v = o.find((v) => { return v.key == key });
 		if (!v) return "";
 		return v.value;
 	};
 
 	static getT(type) {
-		return this.__getTS(type);
+		return gv.__getTS(type);
 	};
 
 	static getALS(type, key) {
-		var o = this.__getTS(type);
+		var o = gv.__getTS(type);
 		return o.find((v) => { return v.key == key }).alias;
 	};
 
 	static getObj(type, key) {
-		var o = this.__getTS(type);
+		var o = gv.__getTS(type);
 		return o.find((v) => { return v.key == key }).value;
 	};
 
 	static getRole(key) {
-		if (!this._role) { this._role = jQuery.parseJSON(localStorage.getItem("role")); }
-		var o = this._role.find((v) => { return v.id == key });
+		if (!gv._role) { gv._role = jQuery.parseJSON(localStorage.getItem("role")); }
+		var o = gv._role.find((v) => { return v.id == key });
 		return (!o) ? "" : o.rolename;
 	};
 
@@ -71,18 +66,18 @@ class gv {
 	};
 
 	static getRes(key) {
-		if (!this._res) { this._res = jQuery.parseJSON(localStorage.getItem("res")); }
-		var o = this._res.find((v) => { return v.id == key });
+		if (!gv._res) { gv._res = jQuery.parseJSON(localStorage.getItem("res")); }
+		var o = gv._res.find((v) => { return v.id == key });
 		return (!o) ? "" : o.name;
 	};
 
 	static getSite(key) {
-		var o = this._site.find((v) => { return v.id == key });
+		var o = gv._site.find((v) => { return v.id == key });
 		return (!o) ? "" : o.sitename;
 	};
 
 	static getArmact(key) {
-		var o = this._armact.find((v) => { return v.id == key });
+		var o = gv._armact.find((v) => { return v.id == key });
 		return (!o) ? "" : o.armactname;
 	};
 
