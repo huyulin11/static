97ad9dfe0ca7d86fa6ac0e370346b63cd25d8a9f
huyulin@calculatedfun.com <huyulin@calculatedfun.com>
2019-09-05 11:08:00 +0800

    ss

--- s/buss/g/j/manager.js
+++ s/buss/g/j/manager.js
@@ -2,6 +2,8 @@ import "/s/buss/acs/g/j/wms.sku.js";
 import "/s/buss/g/j/g.v.js";
 import { gf } from "/s/buss/g/j/g.f.js";
 import "/s/j/tool/urlSearcher.js";
+import "/s/buss/g/j/g.p.js";
+import "/s/buss/g/j/jquery/jquery.autofill.js"
 
 window.nv = {};
 window.nv.loadPage = function (url, openType, sn) {
