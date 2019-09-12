c0e9a4a2862fe6d009d056a4343f93a5417de887
huyulin@calculatedfun.com <huyulin@calculatedfun.com>
2019-09-12 11:38:56 +0800

    盘点功能调整

--- s/buss/wms/j/base/wms.base.add.js
+++ s/buss/wms/j/base/wms.base.add.js
@@ -1,54 +1,68 @@
 import { initRows } from "/s/buss/g/j/dynamic.rows.init.js";
 import "/s/buss/g/j/dynamic.rows.add.js";
 
-var _conf;
+let _tasktype = null;
 
-if (localStorage.projectKey == 'CSY_DAJ') {
-    $("#warehouse").data("notnull", false);
-    _conf = {
-        container: "div#rows",
-        targetClass: "item-group",
-        addBtn: "div.addOne",
-        serial: 0,
-        max: 6,
-        items: [{
-            key: "allocItem",
-            name: "货位名称",
-            notnull: true,
-            type: "associating-input",
-            searchurl: "/alloc/item/findFirstPage.shtml?allocItemFormMap.text=",
-            containerofinput: "#panelBody",
-            showcol: 'text,getStatus(status)'
-        },]
-    }
-} else {
-    $("#targetPlace").parents("div.col").remove();
-    _conf = {
+export var setTasktype = function (tasktype) {
+    _tasktype = tasktype;
+}
+
+export var init = function () {
+    var _conf = {
         container: "div#rows",
         targetClass: "item-group",
         addBtn: "div.addOne",
         serial: 0,
         max: 20,
-        items: [{
-            key: "allocItem",
-            name: "货位名称",
-            notnull: true,
-            type: "associating-input",
-            searchurl: "/alloc/item/findFirstPage.shtml?allocItemFormMap.text=",
-            containerofinput: "#panelBody",
-            showcol: 'text,getStatus(status)',
-        }, {
-            key: "itemcount",
-            name: "数量",
-            notnull: true,
-            default: 1,
-        }, {
-            key: "item",
-            name: "货物种类",
-            notnull: true,
-            default: 1,
-        },]
-    }
-}
+    };
 
-initRows(_conf);
+    if (_tasktype == 'INVENTORY') {
+        _conf.items = [{
+            key: "allocItem",
+            name: "纵号/行号（全盘无需填写）",
+            notnull: true,
+            type: "input",
+        },];
+    } else if (localStorage.projectKey == 'CSY_DAJ') {
+        $("#warehouse").data("notnull", false);
+        let obj = {
+            max: 6,
+            items: [{
+                key: "allocItem",
+                name: "货位名称",
+                notnull: true,
+                type: "associating-input",
+                searchurl: "/alloc/item/findFirstPage.shtml?allocItemFormMap.text=",
+                containerofinput: "#panelBody",
+                showcol: 'text,getStatus(status)'
+            },]
+        }
+        Object.assign(_conf, obj);
+    } else {
+        $("#targetPlace").parents("div.col").remove();
+        let obj = {
+            max: 20,
+            items: [{
+                key: "allocItem",
+                name: "货位名称",
+                notnull: true,
+                type: "associating-input",
+                searchurl: "/alloc/item/findFirstPage.shtml?allocItemFormMap.text=",
+                containerofinput: "#panelBody",
+                showcol: 'text,getStatus(status)',
+            }, {
+                key: "itemcount",
+                name: "数量",
+                notnull: true,
+                default: 1,
+            }, {
+                key: "item",
+                name: "货物种类",
+                notnull: true,
+                default: 1,
+            },]
+        }
+        Object.assign(_conf, obj);
+    }
+    initRows(_conf);
+}
