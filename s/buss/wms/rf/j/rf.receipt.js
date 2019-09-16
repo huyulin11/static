9b676017e13ce2b4407d3eff44daec51bfff9a7e
huyulin@calculatedfun.com <huyulin@calculatedfun.com>
2019-09-16 15:28:40 +0800

    ss

--- s/buss/wms/rf/j/rf.receipt.js
+++ s/buss/wms/rf/j/rf.receipt.js
@@ -3,6 +3,12 @@ import "/s/j/vue/vue.min.js";
 
 let container = "#rootContainer";
 
+var currentPaperid = function () {
+    return localStorage.__receiptPaperId;
+};
+var setCurrentPaperid = function (val) {
+    return localStorage.__receiptPaperId = val;
+};
 export var initRf = function () {
     var vm = new Vue({
         data: {},
@@ -21,17 +27,17 @@ export var initRf = function () {
                 $(container).find("#back").on("click", function () { vm.backMain(); });
                 $(container).find("#execute").on("click", function () { vm.execute(); });
 
-                if (localStorage.receiptPaperId) {
+                if (currentPaperid()) {
                     let url = `/receipt/main/findOneData.shtml`;
-                    gf.ajax(url, { paperid: localStorage.receiptPaperId }, "json", function (s) {
+                    gf.ajax(url, { paperid: currentPaperid() }, "json", function (s) {
                         let main = s.object.main;
                         if (main["status"] != "1" || main["delflag"] != "0") {
-                            layer.msg(localStorage.receiptPaperId + "该单无法继续操作，如需查看请移步入库单管理！");
-                            localStorage.receiptPaperId = "";
+                            layer.msg(currentPaperid() + "该单无法继续操作，如需查看请移步入库单管理！");
+                            setCurrentPaperid("");
                             return;
                         } else {
                             $(container).find("h1").each(function () {
-                                $(this).html("正在入库" + localStorage.receiptPaperId);
+                                $(this).html("正在入库" + currentPaperid());
                             });
                         }
                     });
@@ -39,7 +45,7 @@ export var initRf = function () {
                 $("#su").focus();
             },
             start: function () {
-                if (localStorage.receiptPaperId) {
+                if (currentPaperid()) {
                     layer.msg("当前入库单尚未处理完成，不能再呼叫空车！");
                     return;
                 }
@@ -50,42 +56,42 @@ export var initRf = function () {
                         data = JSON.parse(data);
                         layer.msg(data.msg);
                         if (data.code >= 0) {
-                            localStorage.receiptPaperId = data.object;
+                            setCurrentPaperid(data.object);
                             window.location.reload();
                         }
                     }
                 });
             },
             execute: function () {
-                if (!localStorage.receiptPaperId) {
+                if (!currentPaperid()) {
                     layer.msg("没有生成对应入库单，不能进行执行操作！");
                     return;
                 }
                 gf.doAjax({
-                    url: `/receipt/main/execute.shtml?paperid=${localStorage.receiptPaperId}`,
+                    url: `/receipt/main/execute.shtml?paperid=${currentPaperid()}`,
                     success: function (data) {
                         data = JSON.parse(data);
                         layer.msg(data.msg);
                         if (data.code >= 0) {
-                            localStorage.receiptPaperId = "";
+                            setCurrentPaperid("");
                             window.location.reload();
                         }
                     }
                 });
             },
             cancel: function () {
-                if (!localStorage.receiptPaperId) {
+                if (!currentPaperid()) {
                     layer.msg("没有生成对应入库单，不能进行取消操作！");
                     return;
                 }
                 if (window.confirm("取消操作将删除当前正在操作的入库单，是否继续？")) {
                     gf.doAjax({
-                        url: `/receipt/main/deleteEntity.shtml?paperid=${localStorage.receiptPaperId}`,
+                        url: `/receipt/main/deleteEntity.shtml?paperid=${currentPaperid()}`,
                         success: function (data) {
                             data = JSON.parse(data);
                             layer.msg(data.msg);
                             if (data.code >= 0) {
-                                localStorage.receiptPaperId = "";
+                                setCurrentPaperid("");
                                 window.location.reload();
                             }
                         }
@@ -117,9 +123,9 @@ export var initRf = function () {
                     }
                     return;
                 }
-                if (localStorage.receiptPaperId) {
+                if (currentPaperid()) {
                     gf.doAjax({
-                        url: `/receipt/detail/addItem.shtml?paperid=${localStorage.receiptPaperId}`,
+                        url: `/receipt/detail/addItem.shtml?paperid=${currentPaperid()}`,
                         data: { item: su, userdef3: tu },
                         success: function (data) {
                             layer.msg(data + ":su:" + su + ",tu:" + tu);
