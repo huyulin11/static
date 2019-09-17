9d929b7f6b2e099b400143404a106501c9e1fe80
huyulin@calculatedfun.com <huyulin@calculatedfun.com>
2019-09-17 14:16:02 +0800

    简化方法

--- s/buss/wms/j/base/wms.paper.op.js
+++ s/buss/wms/j/base/wms.paper.op.js
@@ -1,121 +1,129 @@
 import { gf } from "/s/buss/g/j/g.f.js";
 
-function del(url) {
-    var cbox = gf.checkNotNull(window.datagrid);
+var _keyword = null;
+
+function del(that) {
+    var cbox = gf.checkNotNull(window.datagrid, "paperid");
     if (!cbox) { return; }
-    layer.confirm('是否删除？', function (index) {
-        gf.ajax(url, { ids: cbox.join(",") }, "json", function (s) {
+    layer.confirm(`是否${that.name}${cbox}？`, function (index) {
+        gf.ajax(that.url, { paperids: cbox.join(",") }, "json", function (s) {
             layer.msg(s.msg);
             window.datagrid.loadData();
         });
     });
 }
 
-function execute(url) {
-    var cbox = gf.checkOnlyOne(window.datagrid);
-    if (!cbox) { return; }
-    layer.confirm('是否下达执行？（此动作不可撤回）', function (index) {
-        gf.ajax(url, { id: cbox }, "json", function (s) {
-            if (s.code >= 0) {
-                layer.msg('成功下达！');
-                window.datagrid.loadData();
-            } else {
-                layer.msg('下达失败！' + s.msg);
-            }
-        });
-    });
-}
-
-function edit(url) {
+function edit(that) {
     var cbox = gf.checkOnlyOne(window.datagrid, "paperid");
     if (!cbox) { return; }
     window.pageii = layer.open({
-        title: "编辑",
+        title: `${that.name}：` + cbox,
         type: 2,
         area: ["600px", "80%"],
-        content: url + "?paperid=" + cbox
+        content: that.url + "?paperid=" + cbox
     });
 }
 
-function add(url) {
+function add(that) {
     window.pageii = layer.open({
-        title: "新增",
+        title: `${that.name}`,
         type: 2,
         area: localStorage.layerArea.split(","),
-        content: url
+        content: that.url
     });
 }
-function detail(url) {
+
+function detail(that) {
     var cbox = gf.checkOnlyOne(window.datagrid, "paperid");
     if (!cbox) { return; }
     window.pageii = layer.open({
-        title: "明细",
+        title: `${that.name}：` + cbox,
         type: 2,
         area: localStorage.layerArea.split(","),
-        content: url + cbox
+        content: that.url + cbox
     });
 }
-function taked(url) {
+
+function execute(that) {
     var cbox = gf.checkOnlyOne(window.datagrid, "paperid");
     if (!cbox) { return; }
-    layer.confirm('是否接单？', function (index) {
-        gf.ajax(url, { paperid: cbox }, "json", function (s) {
+    layer.confirm(`是否${that.name}${cbox}？`, function (index) {
+        gf.ajax(that.url, { paperid: cbox }, "json", function (s) {
             if (s.code >= 0) {
-                layer.msg('成功接单！');
+                layer.msg(`成功${that.name}！`);
                 window.datagrid.loadData();
             } else {
-                layer.msg('接单失败！' + s.msg);
+                layer.msg(`${that.name}失败！` + s.msg);
             }
         });
     });
 }
 
-var _keyword = null;
+function cancel(that) {
+    var cbox = gf.checkOnlyOne(window.datagrid, "paperid");
+    if (!cbox) { return; }
+    layer.confirm(`是否${that.name}${cbox}？`, function (index) {
+        gf.ajax(that.url, { paperid: cbox }, "json", function (s) {
+            if (s.code >= 0) {
+                layer.msg(`成功${that.name}！`);
+                window.datagrid.loadData();
+            } else {
+                layer.msg(`${that.name}失败！` + s.msg);
+            }
+        });
+    });
+}
+
+function taked(that) {
+    var cbox = gf.checkOnlyOne(window.datagrid, "paperid");
+    if (!cbox) { return; }
+    layer.confirm(`是否${that.name}${cbox}？`, function (index) {
+        gf.ajax(that.url, { paperid: cbox }, "json", function (s) {
+            if (s.code >= 0) {
+                layer.msg(`成功${that.name}！`);
+                window.datagrid.loadData();
+            } else {
+                layer.msg(`${that.name}失败！` + s.msg);
+            }
+        });
+    });
+}
+
 var initPaperOp = function (keyword, rf) {
     _keyword = keyword;
     let btns = null;
+    let addBtn = {
+        id: "add", name: "增加", class: "btn-primary", bind: function () { add(this); },
+        url: `/s/buss/wms/h/${_keyword}AddUI.html`
+    }, editBtn = {
+        id: "edit", name: "修改", class: "btn-primary", bind: function () { edit(this); },
+        url: `/s/buss/wms/h/${_keyword}AddUI.html`
+    }, detailBtn = {
+        id: "detail", name: "明细", class: "btn-primary", bind: function () { detail(this); },
+        url: `/s/buss/wms/h/${_keyword}Details.html?${_keyword}MainFormMap.paperid=`
+    }, executeBtn = {
+        id: "execute", name: "下达", class: "btn-danger", bind: function () { execute(this); },
+        url: `/${_keyword}/main/execute.shtml`
+    }, takedBtn = {
+        id: "taked", name: "接单", class: "btn-primary", bind: function () { taked(this); },
+        url: `/${_keyword}/main/taked.shtml`
+    }, delBtn = {
+        id: "del", name: "删除", class: "btn-danger", bind: function () { del(this); },
+        url: `/${_keyword}/main/deleteEntity.shtml`
+    }, cancelBtn = {
+        id: "cancel", name: "撤销", class: "btn-danger", bind: function () { cancel(this); },
+        url: `/${_keyword}/main/cancel.shtml`
+    };
     if (rf == "RF") {
-        btns = [{
-            id: "detail", name: "明细", class: "btn-primary", bind: function () {
-                detail(`/s/buss/wms/h/${_keyword}Details.html?${_keyword}MainFormMap.paperid=`);
-            }
-        }, {
-            id: "taked", name: "接单", class: "btn-primary", bind: function () {
-                taked(`/${_keyword}/main/taked.shtml`);
-            }
-        },];
+        btns = [addBtn, takedBtn, cancelBtn,];
     } else {
-        btns = [{
-            id: "add", name: "增加", class: "btn-primary", bind: function () {
-                add(`/s/buss/wms/h/${_keyword}AddUI.html`);
-            }
-        }, {
-            id: "del", name: "删除", class: "btn-danger", bind: function () {
-                del(`/${_keyword}/main/deleteEntity.shtml`);
-            }
-        }, {
-            id: "edit", name: "修改", class: "btn-primary", bind: function () {
-                edit(`/s/buss/wms/h/${_keyword}AddUI.html`);
-            }
-        }, {
-            id: "detail", name: "明细", class: "btn-primary", bind: function () {
-                detail(`/s/buss/wms/h/${_keyword}Details.html?${_keyword}MainFormMap.paperid=`);
-            }
-        }, {
-            id: "execute", name: "下达", class: "btn-danger", bind: function () {
-                execute(`/${_keyword}/main/execute.shtml`);
-            }
-        }, {
-            id: "taked", name: "接单", class: "btn-primary", bind: function () {
-                taked(`/${_keyword}/main/taked.shtml`);
-            }
-        },];
+        btns = [addBtn, editBtn, detailBtn, executeBtn, takedBtn, delBtn, cancelBtn,];
     }
     if (_keyword == "inventory") {
-        var whichAgv = function (url) {
+        var whichAgv = function (that) {
             var cbox = gf.checkOnlyOne(window.datagrid, "paperid");
             if (!cbox) { return; }
-            var s = gf.ajax(url, { key: cbox + "%" }, "json");
+            var s = gf.ajax(that.url, { key: cbox + "%" }, "json");
             var info = "";
             for (var item of s) {
                 info = info + "<br/>" + item.key + ":" + item.value;
@@ -124,9 +132,8 @@ var initPaperOp = function (keyword, rf) {
             layer.msg(info);
         }
         btns = btns.concat({
-            id: "whichAgv", name: "执行AGV", class: "btn-info", bind: function () {
-                whichAgv(`/bd/conf.shtml?table=task_agv`);
-            }
+            id: "whichAgv", name: "执行AGV", class: "btn-info", bind: function () { whichAgv(this); },
+            url: `/bd/conf.shtml?table=task_agv`
         });
     }
     gf.bindBtns("div.doc-buttons", btns);
