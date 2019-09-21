cfa061a2fc024383b4de91ce5a44ed1afe94676a
huyulin11@sina.cn <huyulin11@sina.cn>
2019-09-21 22:12:00 +0800

    将绑定的货位信息显示在前台

--- s/buss/wms/j/base/udf/wms.base.add.init.BJJK_HUIRUI.js
+++ s/buss/wms/j/base/udf/wms.base.add.init.BJJK_HUIRUI.js
@@ -7,7 +7,7 @@ export var initUdf = function (_tasktype, _conf) {
             notnull: true,
             type: "input",
         },];
-    } else {
+    } else if (_tasktype == 'receipt') {
         $("#targetPlace").parents("div.col").remove();
         let obj = {
             max: 20,
@@ -19,7 +19,33 @@ export var initUdf = function (_tasktype, _conf) {
                 type: "associating-input",
                 searchurl: "/alloc/item/findFirstPage.shtml?allocItemFormMap.text=",
                 containerofinput: "#panelBody",
-                showcol: 'text,getStatus(status)',
+                showcol: 'text,getStatus(status),num',
+            }, {
+                key: "itemcount",
+                name: "数量",
+                notnull: true,
+                default: 1,
+            }, {
+                key: "item",
+                name: "货物种类",
+                notnull: true,
+                default: 1,
+            },]
+        }
+        Object.assign(_conf, obj);
+    } else if (_tasktype == 'shipment') {
+        $("#targetPlace").parents("div.col").remove();
+        let obj = {
+            max: 20,
+            items: [{
+                key: "allocItem",
+                alias: "userdef1",
+                name: "货位名称",
+                notnull: true,
+                type: "associating-input",
+                searchurl: "/alloc/item/findFirstPage.shtml?allocItemFormMap.inStock=true&allocItemFormMap.text=",
+                containerofinput: "#panelBody",
+                showcol: 'text,getStatus(status),num',
             }, {
                 key: "itemcount",
                 name: "数量",
--- s/buss/wms/j/receipt.main.list.js
+++ s/buss/wms/j/receipt.main.list.js
@@ -2,58 +2,76 @@ import { gv } from "/s/buss/g/j/g.v.js";
 import { dataGrid } from "/s/j/kf.grid.js";
 import { initPaperOp } from "/s/buss/wms/j/base/wms.paper.op.js";
 
+let cols = [{
+	colkey: "id",
+	name: "id",
+	hide: true,
+}, {
+	colkey: "paperid",
+	name: "单号"
+}, {
+	colkey: "warehouse",
+	name: "仓库",
+	renderData: function (rowindex, data, rowdata, column) {
+		if (!data) {
+			return "--空--";
+		}
+		return gv.get("WAREHOUSE", data);
+	}
+}, {
+	colkey: "totallines",
+	name: "明细数"
+}, {
+	colkey: "totalqty",
+	name: "货物数"
+}, {
+	colkey: "name",
+	name: "交接点",
+	renderData: function (rowindex, data, rowdata, column) {
+		return gv.get("ACS_CACHE_CABLE", data);
+	}
+}, {
+	colkey: "status",
+	name: "状态",
+	renderData: function (rowindex, data, rowdata, column) {
+		if (rowdata.delflag == 1) {
+			$("tr[d-tree='" + rowdata.dtee + "']").css("color", "#dedede");
+			return "已删除";
+		} else {
+			if (data == 3) {
+				$("tr[d-tree='" + rowdata.dtee + "']").css("color", "red");
+			}
+		}
+		return gv.get("ACS_STATUS", data);
+	}
+}, {
+	colkey: "updatetime",
+	name: "时间",
+	renderData: function (rowindex, data, rowdata, column) {
+		return "创建：" + new Date(rowdata.createtime).format("yyyy-MM-dd hh:mm:ss") + "<br/>"
+			+ "更新：" + new Date(rowdata.updatetime).format("yyyy-MM-dd hh:mm:ss");
+	}
+}];
+
+if (localStorage.projectKey == "BJJK_HUIRUI") {
+	cols = cols.concat({
+		colkey: "status",
+		name: "目标货位",
+		renderData: function (rowindex, data, rowdata, column) {
+			let s = gf.ajax('/bd/conf.shtml?table=RECEIPT_ALLOC_ITEM', { key: rowdata.paperid }, "json");
+			var info = "";
+			for (var item of s) {
+				info = info + item.value + "<br/>";
+			}
+			if (!info) { info = "未找到执行信息！"; }
+			return info;
+		}
+	});
+}
+
 window.datagrid = dataGrid({
 	pagId: 'paging',
-	l_column: [{
-		colkey: "id",
-		name: "id",
-		hide: true,
-	}, {
-		colkey: "paperid",
-		name: "单号"
-	}, {
-		colkey: "warehouse",
-		name: "仓库",
-		renderData: function (rowindex, data, rowdata, column) {
-			if (!data) {
-				return "--空--";
-			}
-			return gv.get("WAREHOUSE", data);
-		}
-	}, {
-		colkey: "totallines",
-		name: "明细数"
-	}, {
-		colkey: "totalqty",
-		name: "货物数"
-	}, {
-		colkey: "name",
-		name: "交接点",
-		renderData: function (rowindex, data, rowdata, column) {
-			return gv.get("ACS_CACHE_CABLE", data);
-		}
-	}, {
-		colkey: "status",
-		name: "状态",
-		renderData: function (rowindex, data, rowdata, column) {
-			if (rowdata.delflag == 1) {
-				$("tr[d-tree='" + rowdata.dtee + "']").css("color", "#dedede");
-				return "已删除";
-			} else {
-				if (data == 3) {
-					$("tr[d-tree='" + rowdata.dtee + "']").css("color", "red");
-				}
-			}
-			return gv.get("ACS_STATUS", data);
-		}
-	}, {
-		colkey: "updatetime",
-		name: "时间",
-		renderData: function (rowindex, data, rowdata, column) {
-			return "创建：" + new Date(rowdata.createtime).format("yyyy-MM-dd hh:mm:ss") + "<br/>"
-				+ "更新：" + new Date(rowdata.updatetime).format("yyyy-MM-dd hh:mm:ss");
-		}
-	}],
+	l_column: cols,
 	jsonUrl: '/receipt/main/find.shtml',
 	checkbox: true,
 	serNumber: true
