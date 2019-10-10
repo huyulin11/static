c989e98f43878047d57f7bc74661a684b13b28eb
huyulin@calculatedfun.com <huyulin@calculatedfun.com>
2019-10-10 10:38:34 +0800

    出库增加主表明细表数据混合界面

---
+++ s/buss/wms/j/shipment.main.detail.list.js
@@ -0,0 +1,79 @@
+import { gv } from "/s/buss/g/j/g.v.js";
+import { gf } from "/s/buss/g/j/g.f.js";
+import { dataGrid } from "/s/j/kf.grid.js";
+import { initPaperOp } from "/s/buss/wms/j/base/wms.paper.op.js";
+
+let params = {
+	pagId: 'paging',
+	l_column: [{
+		colkey: "id",
+		name: "id",
+		hide: true,
+	}, {
+		colkey: "paperid",
+		name: "单号"
+	}, {
+		colkey: "warehouse",
+		name: "仓库",
+		renderData: function (rowindex, data, rowdata, column) {
+			if (!data) {
+				return "--空--";
+			}
+			return gv.get("WAREHOUSE", data);
+		}
+	}, {
+		colkey: "totallines",
+		name: "明细数"
+	}, {
+		colkey: "totalqty",
+		name: "货物数"
+	}, {
+		colkey: "name",
+		name: "交接点",
+		renderData: function (rowindex, data, rowdata, column) {
+			return gv.get("ACS_CACHE_CABLE", data);
+		}
+	}, {
+		colkey: "status",
+		name: "状态",
+		renderData: function (rowindex, data, rowdata, column) {
+			return gf.getStatusDesc(rowindex, data, rowdata, column);
+		}
+	}, {
+		colkey: "updatetime",
+		name: "时间",
+		renderData: function (rowindex, data, rowdata, column) {
+			return "创建：" + new Date(rowdata.createtime).format("yyyy-MM-dd hh:mm:ss") + "<br/>"
+				+ "更新：" + new Date(rowdata.updatetime).format("yyyy-MM-dd hh:mm:ss");
+		}
+	}],
+	jsonUrl: '/shipment/main/findWithDetail.shtml',
+	checkbox: true,
+	serNumber: true
+}
+
+export var init = function () {
+	let choosedStatus = gf.urlParam("status");
+	if (choosedStatus) {
+		initPaperOp("shipment", "RF");
+		$("html").addClass("frame");
+		params = Object.assign(params, {
+			data: {
+				"shipmentMainFormMap.status": choosedStatus,
+				"shipmentMainFormMap.delflag": 0
+			}
+		});
+	} else {
+		$("#searchForm").show();
+		initPaperOp("shipment");
+	}
+
+	window.datagrid = dataGrid(params);
+}
+
+$("#search").on("click", function () {
+	var searchParams = $("#searchForm").serialize();
+	window.datagrid.setOptions({
+		data: searchParams
+	});
+});
