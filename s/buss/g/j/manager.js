7c17929b7fe7e959b22b1aa47e62d9beeede489a
huyulin@calculatedfun.com <huyulin@calculatedfun.com>
2019-10-15 13:43:05 +0800

    ss

--- s/buss/g/j/manager.js
+++ s/buss/g/j/manager.js
@@ -9,7 +9,7 @@ var _frame = "#loadhtml";
 localStorage.layerArea = ($(window).width() < 960) ? ["95%", "90%"] : ["720px", "80%"];
 gv.init();
 
-$('body').fadeIn(3000);
+$('body').fadeIn();
 var loadPage = function (url, openType, sn) {
     if (sn) {
         var snul = document.getElementById("topli");
@@ -85,5 +85,12 @@ $("a#editUI").on("click", function () {
 
 $(_frame).on("load", function (params) {
     $(this).contents().find("html").addClass("frame");
-    $(this).fadeIn();
+    let key = Math.random(0, 1);
+    if (key > 0.7) {
+        $(this).show();
+    } else if (key > 0.4) {
+        $(this).slideDown();
+    } else {
+        $(this).fadeIn();
+    }
 });
