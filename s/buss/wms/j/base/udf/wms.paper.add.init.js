c8e2d904cbc2b0238b9409601c742d3948ecb3b8
huyulin@calculatedfun.com <huyulin@calculatedfun.com>
2019-10-30 15:46:46 +0800

    ss

--- s/buss/wms/j/base/udf/wms.paper.add.init.js
+++ s/buss/wms/j/base/udf/wms.paper.add.init.js
@@ -1,37 +1,188 @@
-export var initUdf = function (_tasktype, _conf) {
-    if (_tasktype == 'inventory') {
-        _conf.items = [{
-            key: "allocItem",
-            alias: "userdef1",
-            name: "纵号/行号（全盘无需填写）",
-            notnull: true,
-            type: "input",
-        },];
-    } else {
-        $("#targetPlace").parents("div.col").remove();
-        let obj = {
-            max: 20,
-            items: [{
-                key: "allocItem",
-                alias: "userdef1",
-                name: "货位名称",
-                notnull: true,
-                type: "associating-input",
-                searchurl: "/alloc/item/findFirstPage.shtml?allocItemFormMap.text=",
-                containerofinput: "#panelBody",
-                showcol: 'text,getStatus(status)',
-            }, {
-                key: "itemcount",
-                name: "数量",
-                notnull: true,
-                default: 1,
-            }, {
-                key: "item",
-                name: "货物种类",
-                notnull: true,
-                default: 1,
-            },]
+import { renderAll } from "/s/buss/g/j/jquery/jquery.jsSelect.js";
+import { getInput } from "/s/buss/g/j/g.input.render.js";
+
+let _tasktype;
+var _initCols = function () {
+    let _cols;
+    switch (localStorage.projectKey) {
+        case "CSY_DAJ": {
+            if (_tasktype == 'inventory') {
+                _cols = [
+                    { name: "盘点类型", type: "jsSelect", patten: "WMS_INVENTORY_TYPE", notnull: true, key: "inventorytype" },
+                ];
+            } else if (_tasktype == 'receipt') {
+                _cols = [
+                    { name: "目标仓库", type: "jsSelect", patten: "WAREHOUSE", notnull: true, key: "warehouse" },
+                    { name: "出入口", type: "jsSelect", patten: "ACS_CACHE_CABLE", notnull: true, key: "targetPlace", alias: "name" },
+                ];
+            } else if (_tasktype == 'shipment') {
+                _cols = [
+                    { name: "目标仓库", type: "jsSelect", patten: "WAREHOUSE", notnull: true, key: "warehouse" },
+                    { name: "出入口", type: "jsSelect", patten: "ACS_CACHE_CABLE", notnull: true, key: "targetPlace", alias: "name" },
+                ];
+            }
+            break;
         }
-        Object.assign(_conf, obj);
+        case "BJJK_HUIRUI": {
+            if (_tasktype == 'inventory') {
+                _cols = [
+                    { name: "盘点类型", type: "jsSelect", patten: "WMS_INVENTORY_TYPE", notnull: true, key: "inventorytype" },
+                ];
+            } else if (_tasktype == 'receipt') {
+                _cols = [
+                    { name: "目标仓库", type: "jsSelect", patten: "WAREHOUSE", notnull: true, key: "warehouse" },
+                ];
+            } else if (_tasktype == 'shipment') {
+                let isHide = function (a) {
+                    if ($("#warehouse").val() != "1") { $(a).parents("div.col").addClass("hidden"); }
+                    else { $(a).parents("div.col").removeClass("hidden"); }
+                }
+                _cols = [
+                    {
+                        name: "目标仓库", type: "jsSelect", patten: "WAREHOUSE", notnull: true, key: "warehouse",
+                        bind: function (a) {
+                            if ($(a).val() == "1") {
+                                $("#company,#name").val("").parents("div.col").removeClass("hidden");
+                            } else {
+                                $("#company,#name").val("--").parents("div.col").addClass("hidden");
+                            }
+                        }
+                    },
+                    {
+                        name: "拣货点", key: "company", notnull: true, type: "associating-input", defaultValue: "--",
+                        searchurl: "/sys/lap/findFirstPage.shtml?lapInfoFormMap.type=PICK&name=", hide: true,
+                        containerofinput: "#panelBody", showcol: 'name', bind: { event: "init", work: isHide },
+                    },
+                    {
+                        name: "目的地", key: "name", notnull: true, type: "associating-input", defaultValue: "--",
+                        searchurl: "/sys/lap/findFirstPage.shtml?lapInfoFormMap.type=PROD_LINE&name=", hide: true,
+                        containerofinput: "#panelBody", showcol: 'name', bind: { event: "init", work: isHide },
+                    },
+                ];
+            } else if (_tasktype == "transfer") {
+                _cols = [
+                    { name: "源仓库", type: "jsSelect", patten: "WAREHOUSE", notnull: true, key: "sourcewh" },
+                    { name: "目标仓库", type: "jsSelect", patten: "WAREHOUSE", notnull: true, key: "targetwh" },
+                ];
+            }
+            break;
+        }
+        default: {
+            if (_tasktype == 'inventory') {
+                _cols = [
+                    { name: "盘点类型", type: "jsSelect", patten: "WMS_INVENTORY_TYPE", notnull: true, key: "inventorytype" },
+                ];
+            } else if (_tasktype == 'receipt') {
+                _cols = [
+                    { name: "目标仓库", type: "jsSelect", patten: "WAREHOUSE", notnull: true, key: "warehouse" },
+                ];
+            } else if (_tasktype == 'shipment') {
+                let isHide = function (a) {
+                    if ($("#warehouse").val() != "1") { $(a).parents("div.col").addClass("hidden"); }
+                    else { $(a).parents("div.col").removeClass("hidden"); }
+                }
+                _cols = [
+                    {
+                        name: "目标仓库", type: "jsSelect", patten: "WAREHOUSE", notnull: true, key: "warehouse",
+                        bind: function (a) {
+                            if ($(a).val() == "1") {
+                                $("#company,#name").val("").parents("div.col").removeClass("hidden");
+                            } else {
+                                $("#company,#name").val("--").parents("div.col").addClass("hidden");
+                            }
+                        }
+                    },
+                    {
+                        name: "拣货点", key: "company", notnull: true, type: "associating-input", defaultValue: "--",
+                        searchurl: "/sys/lap/findFirstPage.shtml?lapInfoFormMap.type=PICK&name=", hide: true,
+                        containerofinput: "#panelBody", showcol: 'name', bind: { event: "init", work: isHide },
+                    },
+                    {
+                        name: "目的地", key: "name", notnull: true, type: "associating-input", defaultValue: "--",
+                        searchurl: "/sys/lap/findFirstPage.shtml?lapInfoFormMap.type=PROD_LINE&name=", hide: true,
+                        containerofinput: "#panelBody", showcol: 'name', bind: { event: "init", work: isHide },
+                    },
+                ];
+            } else if (_tasktype == "transfer") {
+                _cols = [
+                    { name: "源仓库", type: "jsSelect", patten: "WAREHOUSE", notnull: true, key: "sourcewh" },
+                    { name: "目标仓库", type: "jsSelect", patten: "WAREHOUSE", notnull: true, key: "targetwh" },
+                ];
+            }
+        }
+    }
+    for (let col of _cols) {
+        let obj = $(`<div class="col"><label>${col.name}</label></div>`);
+        obj.append(getInput(col));
+        if (typeof col.hide == "boolean" && col.hide) { obj.addClass("hidden"); }
+        if (typeof col.hide == "function" && col.hide()) { obj.addClass("hidden"); }
+        $("#cols").append(obj);
+    }
+    renderAll();
+}
+
+export var initUdf = function (tasktype, _conf) {
+    _tasktype = tasktype;
+    _initCols();
+
+    switch (localStorage.projectKey) {
+        case "CSY_DAJ": {
+            $("#warehouse").data("notnull", false);
+            let obj = {
+                max: 6,
+                items: [{
+                    key: "allocItem",
+                    alias: "userdef1",
+                    name: "货位名称",
+                    notnull: true,
+                    type: "associating-input",
+                    searchurl: "/alloc/item/findFirstPage.shtml?allocItemFormMap.text=",
+                    containerofinput: "#panelBody",
+                    showcol: 'text,getStatus(status)'
+                },]
+            }
+            Object.assign(_conf, obj);
+            break;
+        }
+        case "BJJK_HUIRUI": {
+            if (_tasktype == 'inventory') {
+                _conf.items = [{
+                    key: "allocItem",
+                    alias: "userdef1",
+                    name: "纵号/行号（全盘无需填写）",
+                    notnull: true,
+                    type: "input",
+                },];
+            } else if (_tasktype == 'receipt') {
+                let obj = {
+                    max: 20,
+                    items: [{
+                        key: "item",
+                        name: "SU",
+                        notnull: true,
+                    }, {
+                        key: "userdef3",
+                        name: "TU",
+                        notnull: true,
+                    },]
+                }
+                Object.assign(_conf, obj);
+            } else if (_tasktype == 'shipment') {
+                let obj = {
+                    max: 20,
+                    items: [{
+                        key: "item",
+                        name: "SU",
+                        notnull: true,
+                    }, {
+                        key: "userdef3",
+                        name: "TU",
+                        notnull: true,
+                    },]
+                }
+                Object.assign(_conf, obj);
+            }
+            break;
+        }
     }
 }
