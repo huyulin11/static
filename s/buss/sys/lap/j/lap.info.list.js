14b6c91eda427c408b21f34e0a53d737e21fed08
ahacs <583937325@qq.com>
2020-05-18 10:59:09 +0800

    增加产线名称修改功能

--- s/buss/sys/lap/j/lap.info.list.js
+++ s/buss/sys/lap/j/lap.info.list.js
@@ -1,6 +1,7 @@
 import { gf } from "/s/buss/g/j/g.f.js";
 import { gv } from "/s/buss/g/j/g.v.js";
 import { dataGrid } from "/s/j/kf.grid.js";
+import { getInput } from "/s/buss/g/j/g.input.render.js";
 import "/s/buss/sys/lap/j/lap.info.edit.name.js";
 
 let tempBtns = [{
@@ -17,18 +18,18 @@ let _columns = [{
 	name: "id",
 	hide: true,
 }, {
+	colkey: "type",
+	name: "类型",
+	renderData: function (rowindex, data, rowdata, column) {
+		return gv.get("LAP_TYPE", data);
+	}
+}, {
 	colkey: "name",
 	name: "名称",
 	renderData: function (rowindex, data, rowdata, column) {
 		return data;
 		// return "<div class='changable'>" + "<span>" + data + "</span>" + "&nbsp;&nbsp;&nbsp;&nbsp;"
 		// 	+ "<a class='editLapName'><img src='/s/i/edit.png'/></a>" + "</div>";
-	}
-}, {
-	colkey: "type",
-	name: "类型",
-	renderData: function (rowindex, data, rowdata, column) {
-		return gv.get("LAP_TYPE", data);
 	}
 }, {
 	colkey: "skuId",
@@ -45,6 +46,7 @@ let _columns = [{
 }, {
 	colkey: "delflag",
 	name: "是否删除",
+	hide: true,
 	renderData: function (rowindex, data, rowdata, column) {
 		if (data == "1") {
 			$("tr[d-tree='" + rowdata.dtee + "']").css("color", "#dcdcdc");
@@ -56,7 +58,7 @@ let _columns = [{
 }];
 
 if (_type == 'PROD_LINE') {
-	let op = {
+	let opStatus = {
 		colkey: "status",
 		name: "状态",
 		renderData: function (rowindex, data, rowdata, column) {
@@ -65,11 +67,27 @@ if (_type == 'PROD_LINE') {
 			}
 			if (!data) { data = "NORMAL"; }
 			let json = { id: rowdata.id, name: rowdata.name };
-			let btnStr = `<button type="button" class="edit btn btn-primary marR10" ${gf.jsonToLabelData(json)}>保存</button>`;
+			let btnStr = `<button type="button" class="editStatus btn btn-primary marR10" ${gf.jsonToLabelData(json)}>保存</button>`;
 			return gv.select("LAP_STATUS", data) + btnStr;
 		}
 	};
-	_columns = _columns.concat(op);
+	let opLineName = {
+		colkey: "name",
+		name: "修改名称",
+		hide: !gf.isPc(),
+		renderData: function (rowindex, data, rowdata, column) {
+			let col = {
+				name: "产线名称", key: "lineName", notnull: true, type: "input",
+			};
+			let json = { id: rowdata.id, name: rowdata.name };
+			let html = getInput(col, { value: data, width: '50%' });
+
+			let btnStr = `<button type="button" class="editLineName btn btn-primary marR10" ${gf.jsonToLabelData(json)}>保存</button>`;
+			return html[0].outerHTML + btnStr;
+		}
+	};
+	_columns = _columns.concat(opLineName);
+	_columns = _columns.concat(opStatus);
 }
 
 window.datagrid = dataGrid({
@@ -80,7 +98,20 @@ window.datagrid = dataGrid({
 	serNumber: true
 });
 
-$("#paging").delegate(".edit", "click", function (e) {
+$("#paging").delegate(".editLineName", "click", function (e) {
+	let name = $(this).data("name");
+	let id = $(this).data("id");
+	let targetVal = $(this).parent("td").find("input#lineName").val();
+	if (name == targetVal) { gf.layerMsg("名称无修改！"); return; }
+	if (window.confirm(`是否要改变该产线(原名称：${name})名称为${targetVal}？`)) {
+		gf.doAjax({
+			url: `/sys/lap/bjjk/huirui/editName.shtml`,
+			data: { id: id, name: targetVal }
+		});
+	}
+});
+
+$("#paging").delegate(".editStatus", "click", function (e) {
 	let id = $(this).data("id");
 	let name = $(this).data("name");
 	let targetVal = $(this).parent("td").find("select").val();
