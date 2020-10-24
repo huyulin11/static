19ae95c3946a798e91e53b5f17b3a3b61a12fa2f
YuZhangkai <1368276952@qq.com>
2020-10-24 10:46:58 +0800

    删除盘点的明细功能

--- s/buss/wms/j/base/wms.paper.op.js
+++ s/buss/wms/j/base/wms.paper.op.js
@@ -88,7 +88,7 @@ var initPaperOp = function (tasktype, optype) {
             break;
         }
         case "YZBD_NRDW": {
-            tempBtns = [btns.add, btns.detail, btns.edit, btns.send, btns.refresh,];
+            tempBtns = [btns.add, btns.edit, btns.send, btns.refresh,];
             if (_tasktype == "inventory") {
                 tempBtns = tempBtns.concat(btns.del, btns.inventoryResult, btns.over);
             }
