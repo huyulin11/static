12df98eae54be01ead996a8de249cd57ceb4c082
YuZhangkai <1368276952@qq.com>
2020-12-01 11:26:04 +0800

    aa

--- s/buss/acs/location/LAO_FOXCONN/location.on.js
+++ s/buss/acs/location/LAO_FOXCONN/location.on.js
@@ -14,11 +14,11 @@ export var move = function () {
         .select("#coordinate")
         .select("svg")
         .on("click", function () {
-            const { layerX, layerY } = d3.event;
+            let x=d3.event.offsetX,y=d3.event.offsetY;
             conf.svg.append("circle")
                 .attr("fill", "blue")
-                .attr("cx", layerX)
-                .attr("cy", layerY)
+                .attr("cx", x)
+                .attr("cy", y)
                 .attr("r", 5)
         })
 }
