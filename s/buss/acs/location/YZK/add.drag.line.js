b81ee20eaff9bde456a953b8ee5bf84e2e028327
YuZhangkai <1368276952@qq.com>
2020-12-08 15:12:10 +0800

    Update add.drag.line.js

--- s/buss/acs/location/YZK/add.drag.line.js
+++ s/buss/acs/location/YZK/add.drag.line.js
@@ -1,46 +1,83 @@
 import { conf } from "/s/buss/acs/location/BASE/location.conf.js";
 import { datas } from "/s/buss/acs/location/BASE/location.data.js";
-import { dbToWindow } from "/s/buss/acs/location/YZK/trans.location.js";
+import { dbToWindow, numToWindow } from "/s/buss/acs/location/YZK/trans.location.js";
 import { updatePoint, dragPoint, addPoint } from "/s/buss/acs/location/LAO_FOXCONN/location.on.js";
 
+export var createPath = function (id, x, y) {
+    for (var i = 1; i <= 4; i++) {
+        newPath(i, x, y);
+    }
+}
 
-export var createLine = function (id, x, y) {
-    var point = getClosestPoint(id, x, y);
-    conf.svg.append("path")
-        .attr("id", "p" + id)
-        .attr("d", function () {
-            let midX = (point.x1 + point.x2) * 0.5;
-            let midY = (point.y1 + point.y2) * 0.5;
-            let s = "M" + point.x1 + "," + point.y1 +
-                "L" + midX + "," + midY +
-                "L" + point.x2 + "," + point.y2;
-            return s;
-        }).attr("class", "clashLine")
+export var removeLine = function (x1, y1, x2, y2) {
+    var distance = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+    if (distance > 100) {
+        d3.selectAll(".post").remove();
+    };
+}
+
+export var startedPath = function () {
+    d3.select(this).attr("d", function () {
+        console.log($(this).attr("d"));
+        return $(this).attr("d");
+    });
+}
+export var dragedPath = function () {
+
+}
+export var endedPath = function () {
+
+}
+
+var newPath = function (num, x, y) {
+    var paths = conf.svg.append("path")
+        .attr("class", "post")
         .attr("fill", "none")
         .attr("stroke", "blue")
         .attr("stroke-width", "2px")
-        .attr("style", "marker-mid:url(#triangle);");
+        .attr("style", "marker-end:url(#triangle);")
+    if (num == 1) {
+        paths.attr("d", function () {
+            return "M" + x + "," + y
+                + "L" + (parseFloat(x) + 20) + "," + y;
+        });
+    }
+    if (num == 2) {
+        paths.attr("d", function () {
+            return "M" + x + "," + y
+                + "L" + (parseFloat(x) - 20) + "," + y;
+        });
+    }
+    if (num == 3) {
+        paths.attr("d", function () {
+            return "M" + x + "," + y
+                + "L" + x + "," + (parseFloat(y) + 20);
+        });
+    }
+    if (num == 4) {
+        paths.attr("d", function () {
+            return "M" + x + "," + y
+                + "L" + x + "," + (parseFloat(y) - 20);
+        });
+    }
 }
+
 var getClosestPoint = function (id, x, y) {
     var arr = [];
     datas.locations.forEach((e, i) => {
         if (e.id != id) {
-            var x1 = e.x;
-            var y1 = e.y;
-            let trans = dbToWindow(x1, y1);
-            x1 = trans[0];
-            y1 = trans[1];
+            var x1 = e.x, y1 = e.y;
+            x1 = dbToWindow(x1, y1)[0];
+            y1 = dbToWindow(x1, y1)[1];
             var value = (x - x1) * (x - x1) + (y - y1) * (y - y1);
             arr.push({ "id": e.id, "value": value });
         }
     });
-    var min = arr[0].value;
-    var idMin = arr[0].id;
+    var min = arr[0].value, idMin = arr[0].id;
     for (var i = 1; i < arr.length; i++) {
         if (min <= arr[i].value);
         else {
-            min = arr[i].value;
-            idMin = arr[i].id;
+            min = arr[i].value, idMin = arr[i].id;
         };
     };
     var point;
