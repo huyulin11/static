18ca397bd22587d0d013bb4f6990cae6d09e3b8a
shizhenhao <43983248+shizhenhao@users.noreply.github.com>
2020-12-28 13:20:25 +0800

    取location值

--- s/buss/acs/location/BASE/location.data.js
+++ s/buss/acs/location/BASE/location.data.js
@@ -58,21 +58,13 @@ var dataLlogic = function (data) {
 }
 
 export var dataLocation = function (data) {
-    for (var index of data) {
-        var val;
-        if (index.location) {
-            val = JSON.parse(index.location);
-        } else val = index;
+    for (var val of data) {
         datas.udfPoints.push([val.id, val.x, val.y]);
         datas.locations.push({ "id": val.id, "x": val.x, "y": val.y });
         datas.id.push({ "id": val.id });
         for (var value of datasLogic) {
             if (val.id === value.siteid) {
-                for (var index2 of data) {
-                    var next;
-                    if (index2.location) {
-                        next = JSON.parse(index2.location);
-                    } else next = index2;
+                for (var next of data) {
                     if (value.nextid === next.id) {
                         datas.point.push({
                             "id": val.id + "" + next.id,
