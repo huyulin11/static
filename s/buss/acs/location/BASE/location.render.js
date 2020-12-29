a20e862ee7350e5bde1035e6daf15b4245996d6a
YuZhangkai <1368276952@qq.com>
2020-12-29 09:29:34 +0800

    箭头定义提到单独文件

--- s/buss/acs/location/BASE/location.render.js
+++ s/buss/acs/location/BASE/location.render.js
@@ -299,25 +299,3 @@ var render = function (tempdata) {
     drawAgvs();
 }
 
-export var markerDef = function () {
-    var defs = conf.defsHome.append("defs");
-    var marker1 = defs.append("marker")
-        .attr("id", "triangle")
-        .attr("markerUnits", "strokeWidth")
-        .attr("markerWidth", 2.5)
-        .attr("markerHeight", 2)
-        .attr("refX", 3.25)
-        .attr("refY", 1)
-        .attr("orient", "auto");
-    marker1.append("path").attr("d", "M 0 0 L 2.5 1 L 0 2 z").attr("fill", "#8a8a8a");
-    var marker2 = defs.append("marker")
-        .attr("id", "triangle2")
-        .attr("markerUnits", "strokeWidth")
-        .attr("markerWidth", 2.5)
-        .attr("markerHeight", 2)
-        .attr("refX", 0)
-        .attr("refY", 1)
-        .attr("orient", "auto");
-    marker2.append("path").attr("d", "M 0 0 L 2.5 1 L 0 2 z").attr("fill", "#8a8a8a");
-
-} 
