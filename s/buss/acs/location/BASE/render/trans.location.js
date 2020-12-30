e1005cc6a95d15042631759ca49b6ab7acf2b14e
YuZhangkai <1368276952@qq.com>
2020-12-30 11:30:42 +0800

    添加单个xy与浏览器与数据库坐标的转换

--- s/buss/acs/location/BASE/render/trans.location.js
+++ s/buss/acs/location/BASE/render/trans.location.js
@@ -5,23 +5,35 @@ conf.yReScale = d3.scaleLinear().domain([0, conf.yAxisWidth]).range(conf.domainY
 
 export var windowToDB = function (id, x, y) {
     var ids = parseInt(id);
-    var x1 = conf.xReScale(x - conf.padding.left);
-    var y1 = conf.yReScale(conf.height - conf.padding.bottom - y);
+    var x1 = xnumToDB(x);
+    var y1 = ynumToDb(y);
     var result = { "id": ids, "x": x1, "y": y1 };
     console.log(result);
     return result;
 }
 
+export var xnumToDB = function (x) {
+    return conf.xReScale(x - conf.padding.left);
+}
+
+export var ynumToDb = function (y) {
+    return conf.yReScale(conf.height - conf.padding.bottom - y);
+}
+
 conf.xScale = d3.scaleLinear().domain(conf.domainXVal).range([0, conf.xAxisWidth]);
 conf.yScale = d3.scaleLinear().domain(conf.domainYVal).range([0, conf.yAxisWidth]);
 
-export var dbToWindow = function (x1,y1) {
-    var x = conf.padding.left + conf.xScale(x1);
-    var y = conf.height - conf.padding.bottom - conf.yScale(y1);
-    var result = [x,y];
-    return  result;
+export var dbToWindow = function (x1, y1) {
+    var x = xnumToWindow(x1);
+    var y = ynumToWindow(y1);
+    var result = [x, y];
+    return result;
 }
 
-export var numToWindow = function (x1) {
-    return conf.padding.left + conf.xScale(x1);
+export var xnumToWindow = function (x) {
+    return conf.padding.left + conf.xScale(x);
+}
+
+export var ynumToWindow = function (y) {
+    return conf.height - conf.padding.bottom - conf.yScale(y);
 }
