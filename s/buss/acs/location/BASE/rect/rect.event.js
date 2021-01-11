d055c95754477535e20a6616a806501e5053fa57
YuZhangkai <1368276952@qq.com>
2021-01-11 15:02:07 +0800

    阻止双击rect新增

--- s/buss/acs/location/BASE/rect/rect.event.js
+++ s/buss/acs/location/BASE/rect/rect.event.js
@@ -1,7 +1,13 @@
 import { addRect } from "/s/buss/acs/location/BASE/rect/rect.add.js";
 import { rightClickRect } from "/s/buss/acs/location/BASE/rect/rect.rightclick.js";
+import { changeRect } from "/s/buss/acs/location/BASE/rect/click.rect.js";
 
 export var rectEvent = function (flag) {
     rightClickRect(flag);
     addRect(flag);
-} 
+    changeRect(flag);
+    d3.selectAll('rect').on('dblclick', function () {
+        d3.event.stopPropagation();
+        return null;
+    })
+}
