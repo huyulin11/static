3bd2c3bccee972b90a1511c010354e43f62551f6
shizhenhao <43983248+shizhenhao@users.noreply.github.com>
2021-01-14 17:18:05 +0800

    地图页面转移

--- s/buss/acs/location/LAO_DBWY/location.p.js
+++
@@ -1,8 +0,0 @@
-﻿import { conf } from "/s/buss/acs/location/BASE/location.conf.js";
-import { tool } from "/s/buss/acs/location/BASE/location.tool.js";
-import { datas } from "/s/buss/acs/location/BASE/location.data.js";
-
-datas.init();
-
-conf.domainYVal = [17500, 46000];
-conf.domainXVal = [28500, 169000];
