0c397d1dff4dd10ed442ed727fd99f92d57e76d8
YuZhangkai <1368276952@qq.com>
2021-01-20 10:43:28 +0800

    站点的添加

---
+++ s/buss/acs/location/location.basepic.js
@@ -0,0 +1,70 @@
+import { datas } from "/s/buss/acs/location/location.data.js";
+import { addSite, getCircleID, createPoint } from "/s/buss/acs/location/point/point.add.js";
+
+export var baseCircleMoudle = function () {
+    var leftShape = d3.select('#shape_panel')
+        .attr('style', "top:80px;left:0px;position:fixed;height: 836px; width: 187px;");
+    var basePictrue = leftShape.append('div')
+        .attr('id', 'panel_basic')
+        .attr('class', 'content')
+        .style('width', '164px');
+    var pictrue = basePictrue.append('div')
+        .attr('class', 'panel_box')
+        .attr('shapename', 'site')
+        .attr('style', 'top:0px;left:0px;display:inline;')
+        .append('img')
+        .attr('id', 'circleimg')
+        .attr('src', '/s/buss/acs/location/redpoint.png')
+        .attr('style', 'width:40px;height:40px;');
+    var rect = basePictrue.append('div')
+        .attr('class', 'panel_box')
+        .attr('shapename', 'build')
+        .attr('style', 'top:0px;left:0px;display:inline;')
+        .append('img')
+        .attr('id', 'rectimg')
+        .attr('src', '/s/buss/acs/location/yellowrect.png')
+        .attr('style', 'width:40px;height:40px;');
+    d3.selectAll('.panel_box').call(
+        d3.drag()
+            .on('start', imgStart)
+            .on('drag', imgDrag)
+            .on('end', imgEnd)
+    );
+}
+
+function imgStart() {
+    datas.init();
+    var name = d3.select(this).attr('shapename');
+    var x = d3.event.x;
+    var y = d3.event.y + 180;
+    if (name == "site") {
+        addSite(x, y);
+    } else if (name == "build") {
+    }
+}
+
+function imgDrag() {
+    var id = getCircleID();
+    var x = d3.event.x;
+    var y = d3.event.y;
+    d3.select('#nc' + id)
+        .attr('cx', x)
+        .attr('cy', y + 180);
+    d3.select('#nr' + id)
+        .attr('x', x)
+        .attr('y', y);
+    d3.select('#nct' + id)
+        .attr('x', x + 7)
+        .attr('y', y + 173);
+    d3.select('#nrt' + id)
+        .attr('x', x)
+        .attr('y', y);
+}
+
+function imgEnd() {
+    var id = getCircleID();
+    var x = event.offsetX;
+    var y = event.offsetY;
+    d3.select('#nrt' + id).remove();
+    createPoint(id, x, y);
+}
---
+++ s/buss/acs/location/point/point.add.js
@@ -0,0 +1,52 @@
+import { datas } from "/s/buss/acs/location/location.data.js";
+import { conf } from "/s/buss/acs/location/location.conf.js";
+import { dragPoint, rightClickPoint } from "/s/buss/acs/location/location.event.js";
+import { addLocation } from "/s/buss/acs/location/url/siteinfo.url.js";
+import { drawPointId } from "/s/buss/acs/location/point/point.draw.js";
+import { tool } from "/s/buss/acs/location/location.tool.js";
+
+export var getCircleID = function () {
+    var ids = datas.id;
+    var id = 1;
+    var arr = [];
+    ids.forEach((e) => {
+        arr.push(e.id);
+    })
+    arr.sort(function (a, b) { return a - b });
+    for (var i of arr) {
+        if (id == i) {
+            id++;
+        }
+    }
+    return id;
+}
+
+export var addSite = function (x, y) {
+    var id = getCircleID();
+    conf.pointHome.append("circle")
+        .attr("id", 'nc' + id)
+        .attr("cx", x)
+        .attr("cy", y)
+        .attr("r", 6.5)
+        .attr("fill", "blue");
+    conf.textHome.append("text")
+        .attr("id", 'nct' + id)
+        .attr("x", x + 7)
+        .attr("y", y - 7)
+        .attr("stroke", "black")
+        .attr("font-size", "15px")
+        .attr("font-family", "sans-serif")
+        .text('' + id);
+}
+
+export var createPoint = function (id, x, y) {
+    var location = tool.windowToDB(id, x, y);
+    addLocation(location, () => {
+        d3.select('#nct' + id).attr('id', 't' + id);
+        d3.select('#nc' + id).attr('id', id);
+        drawPointId(datas.udfPoints);
+        dragPoint(true);
+        rightClickPoint(true);
+    });
+}
+
