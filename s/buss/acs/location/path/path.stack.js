71e6f415ce32c1617581abec9f665f0b6161e2ed
YuZhangkai <1368276952@qq.com>
2021-01-25 15:50:07 +0800

    Update path.stack.js

--- s/buss/acs/location/path/path.stack.js
+++ s/buss/acs/location/path/path.stack.js
@@ -1,11 +1,7 @@
-import { getMPoint } from "/s/buss/acs/location/path/render.d.js";
 import { pathTool } from "/s/buss/acs/location/path/path.tool.js";
-import { getClosestPoint } from "/s/buss/acs/location/path/path.event.add.js";
 import { saveLogic } from "/s/buss/acs/location/url/logic.url.js";
 import { deleteLogic } from "/s/buss/acs/location/url/logic.url.js";
-import { conf } from "/s/buss/acs/location/location.conf.js";
 import { datas } from "/s/buss/acs/location/location.data.js";
-import { tool } from "/s/buss/acs/location/location.tool.js";
 import { markerDef } from "/s/buss/acs/location/path/path.marker.js";
 import { drawPath } from "/s/buss/acs/location/path/path.draw.js";
 import { dragPath, rightClickPath } from "/s/buss/acs/location/location.event.js";
